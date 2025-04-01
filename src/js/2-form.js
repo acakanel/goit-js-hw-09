da93990a8b245fd855b25c9f4c7fe9ef5450efdf
ahmet çakanel <acakanel@gmail.com>
2025-04-01 20:45:51 +0300

    commit ones again

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,23 +1,35 @@
-const form = document.querySelector(".feedback-form");
-let formData = {
-    email: "",
-    message: "",
-};
-const STORAGE_KEY = "feedback-form-state";
-const savedData = localStorage.getItem(STORAGE_KEY);
-if (savedData) {
-    formData = JSON.parse(savedData);
-    form.elements.email.value = formData.email;
-    form.elements.message.value = formData.message;
-}
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    if (!formData.email || !formData.message) {
-        alert("Fill please all fields!");
-        return;
-    }
-    console.log("Submitted data:", formData);
-    localStorage.removeItem(STORAGE_KEY);
-    formData = { email: "", message: "" };
-    form.reset();
+const form = document.querySelector('.feedback-form');
+const formInputEmail = document.querySelector('input[type="email"]');
+const formTextArea = document.querySelector('textarea');
+const localStorageKey = 'feedback-form-state';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const savedData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+  if (savedData.email) formInputEmail.value = savedData.email;
+  if (savedData.message) formTextArea.value = savedData.message;
+});
+
+form.addEventListener('input', () => {
+  const formData = {
+    email: formInputEmail.value.trim(),
+    message: formTextArea.value.trim(),
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const email = formInputEmail.value.trim();
+  const message = formTextArea.value.trim();
+
+  if (!email || !message) {
+    alert('Formda boş alanlar var! Lütfen tüm alanları doldurun.');
+    return;
+  }
+
+  console.log({ email, message });
+
+  localStorage.removeItem(localStorageKey);
+  form.reset();
 });
